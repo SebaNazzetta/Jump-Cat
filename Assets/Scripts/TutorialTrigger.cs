f7584ec8e0346e0fb824a25328ac838a067458e1
SebaNazzetta <seba.nazzetta@gmail.com>
2024-12-10 17:10:25 -0300

    Tutorial placeholder

---
+++ Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject _tutorialPanel;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            _tutorialPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            _tutorialPanel.SetActive(false);
+        }
+    }
+}
