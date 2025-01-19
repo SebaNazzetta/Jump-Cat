8ffe1ced1c5f249dc24ee19e3806d0d48315cc16
genaro-cravero <nicocraveroo@gmail.com>
2025-01-18 21:06:31 -0300

    Sound going to checkpoint fix

--- Assets/Scripts/PlayerSFX.cs
+++ Assets/Scripts/PlayerSFX.cs
@@ -8,24 +8,34 @@ public class PlayerSFX : MonoBehaviour
     [SerializeField] private AudioClip _landSoundFX;
     [SerializeField] private AudioClip _wallHitSoundFX;
     [SerializeField] private AudioClip _hurtSoundFX;
+    private SpriteRenderer _spriteRenderer;
 
+    private void Awake() 
+    {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
 
     public void PlayJumpSoundFX()
     {
+        if (!_spriteRenderer.enabled) return;
         SoundFXManager.instance.PlaySoundFXClip(_jumpSoundFX, transform, 1f);
     }
+
     public void PlayLandSoundFX()
     {
+        if (!_spriteRenderer.enabled) return;
         SoundFXManager.instance.PlaySoundFXClip(_landSoundFX, transform, 1f);
     }
 
     public void PlayWallHitSoundFX()
     {
+        if (!_spriteRenderer.enabled) return;
         SoundFXManager.instance.PlaySoundFXClip(_wallHitSoundFX, transform, 1f);
     }
 
     public void PlayHurtSoundFX()
     {
+        if (!_spriteRenderer.enabled) return;
         SoundFXManager.instance.PlaySoundFXClip(_hurtSoundFX, transform, 1f);
     }
 }
